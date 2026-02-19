d9dfc49c88e167917d10dcc5b2fc80f1e2b945e7
John Shea <sheajohnh@gmail.com>
2026-02-18 22:06:49 -0500

    refactor: Create print_separator function to simplify code

--- bikeshare.py
+++ bikeshare.py
@@ -6,6 +6,9 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+def print_separator(character, quantity):
+    print(character * quantity)
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -25,7 +28,7 @@ def get_filters():
     # get user input for day of week (all, monday, tuesday, ... sunday)
 
 
-    print('-'*40)
+    print_separator('-', 40)
     return city, month, day
 
 
@@ -61,7 +64,7 @@ def time_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print_separator('-', 40)
 
 
 def station_stats(df):
@@ -80,7 +83,7 @@ def station_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print_separator('-', 40)
 
 
 def trip_duration_stats(df):
@@ -96,7 +99,7 @@ def trip_duration_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print_separator('-', 40)
 
 
 def user_stats(df):
@@ -115,7 +118,7 @@ def user_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print_separator('-', 40)
 
 
 def main():
